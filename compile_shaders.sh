191aa7350df8bd6c72df5bb7d301515473ace179
Warwick <warwick.l.e.new@gmail.com>
2022-11-09 15:40:36 +0000

    Added some test shaders started working on constructing a graphics pipeline

---
+++ compile_shaders.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+glslc shaders/shader.vert -o shaders/shader.vert.spv
+glslc shaders/shader.frag -o shaders/shader.vert.spv
---
+++ src/yave_graphics_pipeline.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include<string>
+
+namespace yave {
+
+class YaveGrPipeline {
+  public:
+    YaveGrPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+
+  private:
+    
+};
+
+} // namespace yave
