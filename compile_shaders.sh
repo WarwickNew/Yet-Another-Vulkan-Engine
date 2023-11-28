66af7fc1e8c55c60fb2d1a242c66a1ecdfa28361
Warwick <warwick.l.e.new@gmail.com>
2023-11-28 11:49:09 +0000

    Added the compilation of shaders to the build process.

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 glslc shaders/shader.vert -o shaders/shader.vert.spv
 glslc shaders/shader.frag -o shaders/shader.frag.spv
